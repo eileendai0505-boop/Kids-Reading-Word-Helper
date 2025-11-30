f23e1397e805d05bf3cf26a82740923ca3d222aa
sherry <sherry@sherrydeMacBook-Pro.local>
2025-11-30 16:31:56 +0800

    1

--- src/app/api/groups/[id]/route.ts
+++ src/app/api/groups/[id]/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 
+export const dynamic = 'force-dynamic'
+export const runtime = "nodejs";
+
 export async function PUT(
   request: NextRequest,
   { params }: { params: { id: string } }
